d72b5a8183a80378f7be792a87d49eaf77f15888
aamir <wasi3aamir@gmail.com>
2019-01-28 13:33:55 +0600

    Not Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                double n = double.Parse(space[0]);
+                if (IsperfectSquare(n))
+                {
+                    Console.WriteLine("Case {0}: YES",t);
+                }
+                else
+                {
+                    Console.WriteLine("Case {0}: NO",t);
+                }
+            }
+        }
+        public static bool IsperfectSquare(double n)
+        {
+            int root = (int)Math.Sqrt(n);
+            return (int)Math.Pow(root, 2) == n;
+        }
+    }
+}
